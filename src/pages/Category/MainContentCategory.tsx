9aaff537bd3afc60041965fea458dcc0bbafdd9e
DESKTOP-IK35GA7\Admin <lymphot>
2024-09-29 10:19:59 +0700

    change h content

--- src/pages/Category/MainContentCategory.tsx
+++ src/pages/Category/MainContentCategory.tsx
@@ -2,7 +2,7 @@ import MainContent from "../Home/MainContent";
 
 const MainContainCategory = () => {
   return (
-    <div className="flex w-full h-auto pt-4 min-h-[600px] sm:min-h-[660px] lg:min-h-[670px]">
+    <div className="flex w-full h-auto pt-4 min-h-[600px] sm:min-h-[670px] lg:min-h-[680px]">
       <div className="sm:w-[10px] lg:w-[35%] h-full"></div>
       <div>
         <MainContent />
