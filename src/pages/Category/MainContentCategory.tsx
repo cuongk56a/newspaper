561beb923d0dc17f84ac89296ea8b0d092c2fb7e
DESKTOP-IK35GA7\Admin <lymphot>
2024-09-29 09:28:42 +0700

    min-h content

--- src/pages/Category/MainContentCategory.tsx
+++ src/pages/Category/MainContentCategory.tsx
@@ -4,7 +4,7 @@ const MainContainCategory = () => {
   return (
     <div className="flex w-full h-auto pt-4">
       <div className="sm:w-[10px] lg:w-[35%] h-full"></div>
-      <div>
+      <div  className="min-h-[429px] sm:min-h-[658px] lg:min-h-[670px]">
         <MainContent />
       </div>
       <div className="sm:w-[10px] lg:w-[35%] h-full"></div>
